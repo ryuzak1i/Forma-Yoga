6b08b82f53953b37b3c258f64b7dde7619f649c4
Irelle <irellejann@gmail.com>
2026-08-10 22:24:03 +0800

    Changed forma has img + added working home tab in footer

--- index.js
+++ index.js
@@ -124,15 +124,6 @@
         }
 
         //------------------------COMMUNITY JS------------------------
-        const header = document.querySelector('header');
-
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 50) {
-                header.classList.add('scrolled');
-            } else {
-                header.classList.remove('scrolled');
-            }
-        });
         
         document.querySelectorAll(".swatch").forEach(button => {
             button.addEventListener("click", function () {
